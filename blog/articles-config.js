2e80d6d714d533a8e4dc6d91d55a6e5789d2d5d6
uf4 <3397499417@qq.com>
2025-02-23 14:50:43 +0800

    feat(blog): 测试目录上床

--- blog/articles-config.js
+++ blog/articles-config.js
@@ -2,13 +2,13 @@ const BLOG_ARTICLES = [
     {
         title: "对的.md",
         date: "2024-03-20",
-        path: "/docs/2024-02-22.md",  // 文章的相对路径
+        path: "https://github.com/UF4OVER/UF4OVER.github.io/blob/main/blog/docs/2024-02-23.md",  // 文章的相对路径
         tags: ["标签1", "标签2"]
     },
     {
         title: "热天发热.md",
         date: "2024-03-21",
-        path: "/docs/2024-02-23.md",  // 文章的相对路径
+        path: "https://github.com/UF4OVER/UF4OVER.github.io/blob/main/blog/docs/2024-02-22.md",  // 文章的相对路径
         tags: ["标签3", "标签4"]
     },
 
